ce3d1e8c679d2f2e4fcac4fb58ba90fb5e8e5c22
SimonTheDevloper <simon.b.kurk@gmail.com>
2026-01-20 21:40:37 +0100

    feat: wenn man nun auf die todos klickt werden sie entweder abgehakt oder zu offenen task wiede rgebracht

--- todoManager.js
+++ todoManager.js
@@ -1,18 +1,20 @@
 import { getCompletedTodos, getOpenTodos } from "./script.js";
+import { data } from "./testDaten.js";
 
-const openTodos = getOpenTodos();
-const completedTodos = getCompletedTodos();
-
+console.log(data)
 function renderTodoList(todos, elementId) {
-
+    const liste = document.getElementById(elementId);
+    liste.innerHTML = "" // ganz wichtig löschen damit keine duplicate entsehen!!!!!!!!!!
     todos.forEach(todo => {
-        const liste = document.getElementById(elementId);
         const li = document.createElement('li');
         li.dataset.id = todo._id;
         const label = document.createElement('label');
         const checkbox = document.createElement('input');
         checkbox.type = "checkbox";
         checkbox.checked = todo.completed;
+        checkbox.addEventListener("click", () => {
+            handleTodoClick(todo._id,)
+        });
         const span = document.createElement('span');
         span.textContent = todo.text;
         label.append(checkbox, span);
@@ -20,6 +22,19 @@ function renderTodoList(todos, elementId) {
         liste.appendChild(li);
     });
 }
-renderTodoList(openTodos, 'openTask')
-renderTodoList(completedTodos, 'completedList')
+renderTodoList(getOpenTodos(), 'openTask');
+renderTodoList(getCompletedTodos(), 'completedList');
+
+
+function handleTodoClick(id) {
+    console.log("CLICK ERKANNT! ID:", id);  // ← Sie
+    const todo = data.find(todo => todo._id === id);
+
+    todo.completed = !todo.completed;
+    console.log("Rendere Listen...");
+    renderTodoList(getOpenTodos(), 'openTask');
+    renderTodoList(getCompletedTodos(), 'completedList');
+}
+
+handleTodoClick('696cf50bb7222e1da92147b6');
 
