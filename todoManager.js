d5d8dd4337ae85c6fac8e577cf7f5ebdc6296272
SimonTheDevloper <simon.b.kurk@gmail.com>
2026-01-20 19:21:47 +0100

    renderer vereinfacht da man nun den die zwei gefilterten objekte als parameter mitschickt

--- todoManager.js
+++ todoManager.js
@@ -3,12 +3,12 @@ import { getCompletedTodos, getOpenTodos } from "./script.js";
 const openTodos = getOpenTodos();
 const completedTodos = getCompletedTodos();
 
+function renderTodoList(todos, elementId) {
 
-
-function renderTodos() {
-    const openTaskliste = document.getElementById('openTask');
-    openTodos.forEach((todo => {
+    todos.forEach(todo => {
+        const liste = document.getElementById(elementId);
         const li = document.createElement('li');
+        li.dataset.id = todo._id;
         const label = document.createElement('label');
         const checkbox = document.createElement('input');
         checkbox.type = "checkbox";
@@ -17,23 +17,9 @@ function renderTodos() {
         span.textContent = todo.text;
         label.append(checkbox, span);
         li.appendChild(label);
-        openTaskliste.appendChild(li);
-    }));
+        liste.appendChild(li);
+    });
+}
+renderTodoList(openTodos, 'openTask')
+renderTodoList(completedTodos, 'completedList')
 
-    const completedTaskListe = document.getElementById('completedList');
-
-    completedTodos.forEach((todo => {
-        const li = document.createElement('li');
-        const label = document.createElement('label');
-        const checkbox = document.createElement('input');
-        checkbox.type = "checkbox";
-        checkbox.checked = todo.completed;
-        const span = document.createElement('span');
-        span.textContent = todo.text;
-        label.append(checkbox, span);
-        li.appendChild(label);
-        completedTaskListe.appendChild(li);
-    }));
-
-};
-renderTodos();
