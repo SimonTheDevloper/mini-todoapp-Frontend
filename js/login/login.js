eec4abafcb3eaf47ce609cd2dd501910d2685914
SimonTheDevloper <simon.b.kurk@gmail.com>
2026-01-24 15:06:02 +0100

    feat: beim signUp bekommt man nun bessages zurück ob es geklappt hat oder nicht

--- js/login/login.js
+++ js/login/login.js
@@ -1,37 +1,73 @@
-import { createNewUser } from "./loginServices.js";
+import { createNewUser, loginUser } from "./loginServices.js";
 
 const signUpForm = document.getElementById('signUpForm');
 const logInForm = document.getElementById('logInForm');
+
+const loginMsg = document.getElementById('loginMsg');
+const signUpMsg = document.getElementById('signUpMsg')
+
 const switchToSignUp = document.getElementById('switchToSignUp');
 const switchToLogIn = document.getElementById('switchToLogIn');
 
 switchToSignUp.addEventListener('click', () => {
-    logInForm.style.display = "none";
-    signUpForm.style.display = "block";
+  logInForm.style.display = "none";
+  signUpForm.style.display = "block";
+  loginMsg.textContent = ""
 });
 switchToLogIn.addEventListener('click', () => {
-    signUpForm.style.display = "none";
-    logInForm.style.display = "block";
-});
-logInForm.addEventListener('submit', (e) => {
-    e.preventDefault() // damit die seite nicht dadurch neu läd
-    const formData = new FormData(logInForm);
-    const user = {
-        email: formData.get('email'),
-        password: formData.get('password')
-    }
-    console.log(user)
-});
-signUpForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const formData = new FormData(signUpForm);
-    const newUser = {
-        username: formData.get('username'),
-        email: formData.get('email'),
-        password: formData.get('password')
-    }
-    console.log(newUser);
-    createNewUser(newUser)
-    signUpForm.reset();
-})
+  signUpForm.style.display = "none";
+  logInForm.style.display = "block";
+  signUpMsg.textContent = "";
 
+});
+
+
+logInForm.addEventListener('submit', handleLogin);
+
+async function handleLogin(e) {
+  e.preventDefault() // damit die seite nicht dadurch neu läd
+  const formData = new FormData(logInForm);
+  const user = {
+    email: formData.get('email'),
+    password: formData.get('password')
+  }
+  updateMsg(loginMsg, 'login to account...', 'black');
+  try {
+
+  } catch (err) {
+
+  }
+  console.log(user);
+  loginUser(user)
+  logInForm.reset();
+};
+
+
+signUpForm.addEventListener('submit', handleSignUp)
+
+async function handleSignUp(e) {
+  e.preventDefault();
+
+  const formData = new FormData(signUpForm);
+  const newUser = {
+    username: formData.get('username'),
+    email: formData.get('email'),
+    password: formData.get('password')
+  };
+
+  updateMsg(signUpMsg, 'Creating account...', 'black');
+
+  try {
+    await createNewUser(newUser);
+    updateMsg(signUpMsg, 'Successfully created', 'green');
+    signUpForm.reset();
+  } catch (err) {
+    updateMsg(signUpMsg, err.message || 'Error', 'red');
+
+  }
+};
+
+function updateMsg(elemt, text, color) {
+  elemt.textContent = text;
+  elemt.style.color = color;
+};
--- js/login/loginServices.js
+++ js/login/loginServices.js
@@ -2,16 +2,38 @@ const url = "http://localhost:3000/user"
 
 export async function createNewUser(newUser) {
     try {
-        await fetch(url, {
+        const response = await fetch(`${url}/register`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ newUser })
+            body: JSON.stringify(newUser),
+            credentials: 'include',
         })
-        const data = await response.JSON();
+        const data = await response.json();
         console.log(data)
     } catch (err) {
         console.error(err)
+    }
+}
+export async function loginUser(user) {
+    try {
+        const response = await fetch(`${url}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user),
+            credentials: 'include',
+        })
+        const data = await response.json();
+        console.log(data);
+        if (!response.ok) {
+            throw new Error(data.message || "Registration failed");
+        }
+        return data
+    } catch (err) {
+        console.error('created user error:', err);
+        throw new Error(err.message || 'Failed to create user')
     }
 }
