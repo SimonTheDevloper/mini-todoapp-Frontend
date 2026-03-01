e64354c36121d02e10649614d96c08d4587d460f
SimonTheDevloper <simon.b.kurk@gmail.com>
2026-03-01 12:52:21 +0100

    feat: bearbeiten butten jetzt neben den löschen button plaziert

--- js/todos/todoUI.js
+++ js/todos/todoUI.js
@@ -1,46 +1,50 @@
 import { updateEmptyStates } from "./todoManager.js";
 
 export function renderTodoList(todos, elementId, clickHandler) {
-    // jetzt clickhandler als callbackk
     const liste = document.getElementById(elementId);
-    liste.innerHTML = ""; // ganz wichtig löschen damit keine duplicate entsehen!!!!!!!!!!
+    liste.innerHTML = "";
+
     todos.forEach((todo) => {
         const li = document.createElement("li");
         li.dataset.id = todo._id;
-        li.className =
-            "flex items-center p-2 py-2 bg-base-100 rounded-lg border-1 border-gray-100 hover:border-gray-200 cursor-pointer";
+        li.className = "flex items-center p-2 py-2 bg-base-100 rounded-lg border-1 border-gray-100 hover:border-gray-200 cursor-pointer";
+
         const label = document.createElement("label");
         label.className = "flex gap-2";
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
-        checkbox.className = "checkbox ";
+        checkbox.className = "checkbox";
         checkbox.checked = todo.completed;
         checkbox.addEventListener("change", () => {
             clickHandler(todo._id);
         });
+
         const span = document.createElement("span");
         span.textContent = todo.text;
         span.className = "text-gray-700 flex-1";
 
         const editBtn = document.createElement("button");
         editBtn.dataset.id = todo._id;
-        editBtn.className =
-            "btn btn-sm btn-ghost opacity-50 hover:opacity-100 transition-opacity ml-auto edit";
+        editBtn.className = "btn btn-sm btn-ghost opacity-50 hover:opacity-100 transition-opacity edit";
         const editIcon = document.createElement("i");
         editIcon.className = "fa-solid fa-pen-to-square";
         editBtn.appendChild(editIcon);
+
         const delteBtn = document.createElement("button");
         delteBtn.dataset.id = todo._id;
-        delteBtn.className =
-            "btn btn-sm btn-ghost text-error opacity-50 hover:opacity-100 transition-opacity ml-auto";
+        delteBtn.className = "btn btn-sm btn-ghost text-error opacity-50 hover:opacity-100 transition-opacity";
         const trashIcon = document.createElement("i");
         trashIcon.className = "fa-solid fa-trash-can";
         delteBtn.appendChild(trashIcon);
 
-        label.append(checkbox, span);
-        li.append(label, delteBtn, editBtn);
-        liste.appendChild(li);
+        const btnContainer = document.createElement('div');
+        btnContainer.className = "flex gap-2 ml-auto";
+        btnContainer.append(editBtn, delteBtn);
 
-        updateEmptyStates();
+        label.append(checkbox, span);
+        li.append(label, btnContainer);
+        liste.appendChild(li);
     });
+
+    updateEmptyStates();
 }
